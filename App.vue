0eb0008d224210ab95258e09bd39db61df61d117
李瑞孟 <66002375+raymondk25@users.noreply.github.com>
2020-07-10 16:23:22 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+</style>
